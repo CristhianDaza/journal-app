61fec30322984612b76ccdd678944117c54727c2
Cristhian Daza <cristian.d.b@hotmail.com>
2021-09-03 22:50:50 -0500

    Creando ruta de dayBook

---
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>BaybookLayout</h1>
+</template>
+
+<script>
+export default {
+  name: 'DayBookLayout'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import daybookRouter from '@/modules/daybook/router'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
+    path: '/daybook',
+    ...daybookRouter
   }
 ]
 
