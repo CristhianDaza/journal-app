5a13896ca04137d5277dc338a1b40d5f91e9bb26
Cristhian Daza <cristian.d.b@hotmail.com>
2021-09-04 22:35:12 -0500

    Mostrando la entrada en el componente principal

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -2,3 +2,9 @@ export const getEntriesByTerm = ({ entries }) => (term = '') => {
   if (term.length === 0) return entries
   return entries.filter(entry => entry.text.toLowerCase().includes(term.toLowerCase()))
 }
+
+export const getEntryById = ({ entries }) => (id = '') => {
+  const entry = entries.find(entry => entry.id === id)
+  if (!entry) return
+  return { ...entry }
+}
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="entry-title d-flex justify-content-between p-2">
     <div>
-      <span class="text-success fs-3 fw-bold">15</span>
-      <span class="mx-1 fs-3">Septiembre</span>
-      <span class="mx-2 fs-4 fw-light">2021, sabado</span>
+      <span class="text-success fs-3 fw-bold">{{ day }}</span>
+      <span class="mx-1 fs-3">{{ mount }}</span>
+      <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
     </div>
     <div>
       <button class="btn btn-danger mx-2">
@@ -19,6 +19,7 @@
   <hr>
   <div class="d-flex flex-column px-3 h-75">
     <textarea
+      v-model="entry.text"
       placeholder="¿Qué sucedió hoy?"
     ></textarea>
   </div>
@@ -34,10 +35,49 @@
 
 <script>
 import { defineAsyncComponent } from 'vue'
+import { mapGetters } from 'vuex'
+import getDayMonthYear from '../helpers/getDayMonthYear'
+
 export default {
   name: 'EntryView',
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data () {
+    return {
+      entry: null
+    }
+  },
   components: {
     Fab: defineAsyncComponent(() => import('../components/Fab'))
+  },
+  methods: {
+    loadEntry () {
+      const entry = this.getEntryById(this.id)
+      if (!entry) this.$router.push({ name: 'no-entry' })
+      this.entry = entry
+    }
+  },
+  computed: {
+    ...mapGetters('journalModule', ['getEntryById']),
+    day () {
+      const { day } = getDayMonthYear(this.entry.date)
+      return day
+    },
+    mount () {
+      const { mount } = getDayMonthYear(this.entry.date)
+      return mount
+    },
+    yearDay () {
+      const { yearDay } = getDayMonthYear(this.entry.date)
+      return yearDay
+    }
+  },
+  created () {
+    this.loadEntry()
   }
 }
 </script>
