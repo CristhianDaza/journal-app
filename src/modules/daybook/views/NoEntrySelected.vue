afb6cac410a92ab6a8c760093d7c76881f9ef230
Cristhian Daza <cristian.d.b@hotmail.com>
2021-09-04 19:27:33 -0500

    Creando componente de NoEntrySelected

---
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <h1 class="mt-5">No hay nada seleccionado</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NoEntrySelected'
+}
+</script>
+
+<style lang="scss" scoped>
+  .div {
+    height: 100%;
+  }
+</style>
