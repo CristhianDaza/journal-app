7f5bc283b2af2f27ca1901ed0e7267380a4711fd
Cristhian Daza <cristian.d.b@hotmail.com>
2021-09-04 21:17:43 -0500

    Creando datos de prueba

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,29 @@
 export default () => ({
   isLoading: true,
-  entries: []
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam auctor est in fringilla congue. Etiam ut lectus tempus, mollis nunc sit amet, vestibulum nulla. Duis ornare consectetur luctus. Phasellus eu aliquet libero, eu efficitur erat. Cras ac magna et ipsum consequat scelerisque. Donec quis dolor sapien. Phasellus id nunc sed magna finibus aliquet. Praesent non mauris consequat, sodales justo sit amet, facilisis nisi. Morbi et purus et justo viverra tincidunt. Integer sodales justo vitae risus lacinia, et lacinia tortor euismod. Cras tincidunt et augue id auctor. Aenean condimentum nec dui non volutpat.',
+      picture: null
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: 'Pellentesque tincidunt lacus lorem, at faucibus justo convallis eget. Etiam viverra ligula eget dui sagittis, eget cursus purus accumsan. Sed vitae commodo nunc, in accumsan nulla. Nulla ultrices lectus id feugiat porta. Suspendisse dignissim, nisi nec auctor dictum, lectus nisi sollicitudin leo, et porta ipsum neque non est. Quisque in euismod leo. Nam vel consequat sapien. Ut consectetur lectus nec velit commodo condimentum. Integer congue volutpat tortor sed tristique. Curabitur non massa varius, fringilla arcu non, tristique quam. Sed fermentum, erat venenatis viverra convallis, nulla felis convallis lectus, eget varius nulla nibh sit amet ipsum. Nulla vitae egestas augue.',
+      picture: null
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: 'Curabitur sollicitudin luctus nulla. Aenean tristique sapien sed ultricies laoreet. Integer ornare massa et ultrices cursus. Ut sagittis erat quis diam gravida, nec malesuada orci pharetra. Praesent varius lectus sapien, gravida rutrum nisl volutpat ut. Ut efficitur feugiat volutpat. Nam sollicitudin semper elit lacinia pharetra. Donec finibus volutpat tellus. Nulla ornare magna sit amet mattis fringilla. Curabitur lacinia lacus at elit placerat, eu suscipit massa maximus.',
+      picture: null
+    },
+    {
+      id: new Date().getTime() + 3000,
+      date: new Date().toDateString(),
+      text: 'Etiam porttitor rutrum est, nec tempus purus sagittis at. Phasellus a gravida mauris, nec facilisis ipsum. Donec turpis mauris, semper vitae ligula et, vehicula tempor libero. Cras congue facilisis lectus. Cras placerat vehicula efficitur. Integer fringilla rutrum nulla eget blandit. Donec tristique felis eget euismod sagittis. Etiam mauris lorem, rhoncus quis augue ut, tincidunt varius nisl. Vestibulum et lorem vulputate, cursus turpis sit amet, sodales arcu. Aliquam mi tortor, bibendum eu sem quis, convallis rhoncus purus. Sed euismod nibh ipsum, eu pellentesque sapien porta ac. Duis quis leo vitae mauris porttitor posuere.',
+      picture: null
+    }
+  ]
 })
